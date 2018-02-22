38441b3046e070a9157889e245225cbedd2958af
Erick1994Intecap <erickruiz1994@gmail.com>
2018-02-22 13:49:09 -0600

    Commit

--- app/src/main/java/com/aesc/santos/gitanoapp/LoginActivity.java
+++ app/src/main/java/com/aesc/santos/gitanoapp/LoginActivity.java
@@ -7,7 +7,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,20 +28,29 @@ public class LoginActivity extends AppCompatActivity {
     private Button send;
     private TextView reigs;
 
+    EditText txtUsuario, txtPassword;
+    Button btnIngresar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        txtUsuario = (EditText)findViewById(R.id.txtDpi);
+        txtPassword = (EditText)findViewById(R.id.txtContaseña);
+         btnIngresar = (Button)findViewById(R.id.btnLogin);
+
+
         send = findViewById(R.id.btnLogin);
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "onClick: stating");
-                Intent intent = new Intent(getApplicationContext(),BodyActivity.class);
+                //Log.d(TAG, "onClick: stating");
+               // Intent intent = new Intent(getApplicationContext(),BodyActivity.class);
                 //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
+                //startActivity(intent);
+                ConsultaPass("http://adrax.hol.es/php/consultarusuario.php?user="+txtPassword.getText().toString());
             }
         });
 
@@ -43,4 +64,50 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    private void ConsultaPass(String URL) {
+
+        Log.i("url",""+URL);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest =  new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    JSONArray ja = new JSONArray(response);
+                    String contra = ja.getString(0);
+                    if(contra.equals(txtPassword.getText().toString())){
+
+                        Toast.makeText(getApplicationContext(),"Bienvenido",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(),BodyActivity.class);
+                        startActivity(intent);
+
+                    }else{
+                        Toast.makeText(getApplicationContext(),"verifique su contraseña",Toast.LENGTH_SHORT).show();
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                    Toast.makeText(getApplicationContext(),"El usuario no existe en la base de datos",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(stringRequest);
+
+
+
+    }
+
 }
